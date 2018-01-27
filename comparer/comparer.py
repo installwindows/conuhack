7f99a6e2d03022d0d726260b6cd7a4065ca863c1
killerzdouou <charles.menard@live.fr>
2018-01-27 18:48:56 -0500

    comparer fonctionne

--- comparer/comparer.py
+++ comparer/comparer.py
@@ -1,19 +1,23 @@
 from PIL import Image
 import numpy as np
 from skimage import data, img_as_float
-from skimage.measure import compare_nrmse
+from skimage.measure import compare_ssim
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 #return numpy array of the image
-def load_image(infilename, width, heigth ) :
+def load_image(infilename, width, height ) :
     img = Image.open(infilename)
-    img.load()
-    data = np.asarray(img, dtype="int32" )
+
+    img = img.resize((width, height), Image.NEAREST)
+    data = img_as_float(img)
     return data
 
-im1 = load_image("b.jpeg", 300, 200)
-#print(im1)
-im2 = load_image("c.jpeg", 300, 200)
-diff = compare_nrmse(im1, im2)
-print(diff)
+im1 = load_image("a.jpeg", 300, 200)
+im2 = load_image("b.jpeg", 300, 200)
+
+plt.figure()
+plt.imshow(im2)
+plt.show()
+
+print(compare_ssim(im1, im2, multichannel=True))
