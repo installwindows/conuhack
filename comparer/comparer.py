946cbd21236effb222b1fa65c64ddd73f0724548
killerzdouou <charles.menard@live.fr>
2018-01-27 18:21:25 -0500

    comparer

--- comparer/comparer.py
+++ comparer/comparer.py
@@ -1,18 +1,19 @@
 from PIL import Image
-from numpy import array
+import numpy as np
+from skimage import data, img_as_float
+from skimage.measure import compare_nrmse
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 #return numpy array of the image
-def importAndResize(filename, width, height):
-    im = Image.open(filename)
+def load_image(infilename, width, heigth ) :
+    img = Image.open(infilename)
+    img.load()
+    data = np.asarray(img, dtype="int32" )
+    return data
 
-    im = im.resize((width, height), Image.BILINEAR)      # use nearest neighbour
-    arr = array(im)
-
-    return im
-
-print(importAndResize("a.jpeg", 300, 200))
-plt.figure()
-plt.imshow(importAndResize("a.jpeg", 300, 200))
-plt.show()  # display it
+im1 = load_image("b.jpeg", 300, 200)
+#print(im1)
+im2 = load_image("c.jpeg", 300, 200)
+diff = compare_nrmse(im1, im2)
+print(diff)
