97fb20df1efcbfdccfff2bd41d989899eb31e315
killerzdouou <charles.menard@live.fr>
2018-01-27 20:47:35 -0500

    formatclassifier

--- comparer/comparer.py
+++ comparer/comparer.py
@@ -7,16 +7,16 @@ import matplotlib.image as mpimg
 from sklearn.decomposition import PCA
 
 #return numpy array of the image
-def load_image(infilename, width, height ) :
+def load_image(infilename, width, height) :
     img = Image.open(infilename)
 
     img = img.resize((width, height), Image.NEAREST)
     data = img_as_float(img)
     return data
 
-def comparerDeuxImages(im1, im2, pcaPourcentage):
-    model = PCA()
-    pca1 = model.fit(im1.flatten())
-    pca2 = model.fit(im2.flatten())
 
-    return compare_ssim(im1, im2, multichannel=True)
+
+
+#im1 = load_image("a.jpeg", 300, 200)
+#im2 = load_image("b.jpeg", 300, 200)
+#print(compare_ssim(im1, im2, multichannel=True))
---
+++ formatclassifier.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def classifyFormatOfPict(filename):
+    im = Image.open(filename)
+    width, height = im.size
+    ratio = width/height
+    print(ratio)
+    if(abs(ratio-1.5) < abs(ratio-0.66)):
+        return im.resize((int(width*1.5),width), Image.NEAREST)
+    else :
+        return im.resize((width, int(width*1.5)),Image.NEAREST)
+
+im = classifyFormatOfPict("a.jpg")
+
+plt.figure()
+plt.imshow(im)
+plt.show()
